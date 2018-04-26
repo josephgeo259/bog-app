24b1e56b55e6dd9e3410c1729d77dac0fd0a4b24
GeoJoseph <josephgeo259@gmail.com>
2018-04-26 18:55:44 -0400

    commits changes to alot and scritpts

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+mongoose.connect(process.env.MONGODB_URI)
 
 const db = mongoose.connection
 db.on('error', err => {
@@ -22,15 +22,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
-const creatureRoutes = require('./routes/creatureController')
-app.use('/api/creatures', creatureRoutes)
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+// const creatureRoutes = require('./controllers/creatureController')
+// app.use('/api/creatures', creatureRoutes)
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
+    res.send(`hello world`)
 })
 
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log('App is up and running on port ' + PORT)
+    console.log('Server is running' + PORT)
 })
